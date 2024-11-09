ee61567d03f0199ea9e0e9c30ff54c09ac931f77
Aimararivv <xitziafierro@gmail.com>
2024-11-09 00:14:16 -0700

    listar, editar y eliminar

---
+++ libreriaXAGFRRR/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using libreriaXAGFRRR.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace libreriaXAGFRRR.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        
+        }
+        public DbSet<Books> Books { get; set; }
+    }
+}
---
+++ libreriaXAGFRRR/Data/Models/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace libreriaXAGFRRR.Data.Models
+{
+    public class Books
+    {
+        public int id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public bool ISRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genero { get; set; }
+        public string Autor { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
